ffc6d6f996c7302ec961b4bcfd041648b1881b9c
jangkyoungtae <zxs3710@naver.com>
2022-07-05 17:45:20 +0900

    [Test] PWA Notify Test

--- src/Pages/MainPage/MainPageContainer.tsx
+++ src/Pages/MainPage/MainPageContainer.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable no-alert */
 import { useEffect, useState } from 'react';
 import { useQuery } from 'react-query';
@@ -11,6 +12,7 @@ export default function MainPageContainer() {
   const recommendPosts = useQuery('recommend_post', () => postApi.getRecommendPosts());
   const [isLoading, setLoding] = useState<boolean>(false);
   const testPostNotify = () => {
+    console.log('test');
     Notification.requestPermission().then((status) => {
       if (status === 'denied') {
         alert('Notification 거부됨');
