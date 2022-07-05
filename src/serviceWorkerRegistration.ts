df2ed8390d3f1f3464a92adb1c9cc7aee5032f66
jangkyoungtae <zxs3710@naver.com>
2022-07-05 17:55:25 +0900

    [Test] PWA Notify Test

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -110,7 +110,7 @@ function registerValidSW(swUrl: string, config?: Config) {
               // push subscription이 유저에게 항상 보이는지 여부.
               // 알림을 숨기는 등 작업이 들어가지는에 대한 여부인데, 크롬에서는 true 밖에 지원안한다.
               // https://developers.google.com/web/fundamentals/push-notifications/subscribing-a-user
-              applicationServerKey: process.env.REACT_APP_PUSH_PUB_KEY, // 발급받은 vapid public key
+              applicationServerKey: 'BI6IgSHcefeJithMgtfCnG5D4jJFuhJM7d182OCibSAZjZRxXPxqUUazXW25z_NqGZ_8zXp5RXT4DiwqNfMsFks', // 발급받은 vapid public key
             };
 
             return registration.pushManager.subscribe(subscribeOptions);
