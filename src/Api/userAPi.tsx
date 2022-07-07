09823314d0ea94880487cff311ed747000ad6e27
eunji <harper9808@gmail.com>
2022-07-07 19:35:43 +0900

    [set] baseUrl 수정

--- src/Api/userAPi.tsx
+++ src/Api/userAPi.tsx
@@ -1,9 +1,16 @@
 import baseUrl from './baseUrl';
 
+//소셜 로그인 후 ??
 const getUser = async () => {
   const res = await baseUrl.get(`/user`);
   return res;
 };
+//프로필 변경
+const putUser = async () => {
+  const res = await baseUrl.put(`/user`);
+  return res;
+};
+
 const getUserProfile = async (email: string) => {
   const res = await baseUrl.get(`/user/${email}`);
   return res;
@@ -11,5 +18,6 @@ const getUserProfile = async (email: string) => {
 
 export default {
   getUser: () => getUser(),
+  putUser: () => putUser(),
   getUserProfile: (email: string) => getUserProfile(email),
 };
