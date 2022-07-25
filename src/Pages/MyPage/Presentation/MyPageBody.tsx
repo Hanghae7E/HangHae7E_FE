57433b5fd94934d7e170f509daaa5f29e29d41e9
jangkyoungtae <zxs3710@naver.com>
2022-07-25 18:54:43 +0900

    [Feat] develop Mypage Merge conflict 수정

--- src/Pages/MyPage/Presentation/MyPageBody.tsx
+++ src/Pages/MyPage/Presentation/MyPageBody.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useState } from 'react';
 import { useMutation, useQueryClient } from 'react-query';
 import { IProfileFormData, IsideProfile } from '../../../TypeInterface/userType';
