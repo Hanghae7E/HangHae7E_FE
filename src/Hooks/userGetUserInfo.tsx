0e9f5467aab19615defcf605542c83c540f6e07c
jangkyoungtae <zxs3710@naver.com.com>
2022-08-04 13:34:49 +0900

    [Fix] 콘솔제거

--- src/Hooks/userGetUserInfo.tsx
+++ src/Hooks/userGetUserInfo.tsx
@@ -6,11 +6,9 @@ import userApi from '../Api/userApi';
 import jwtUtils from '../util/JwtUtil';
 
 export default () => {
-  console.log('get_userInfo1');
   const token = localStorage.getItem('token');
   if (token) {
     const userId = jwtUtils.getId(token);
-    console.log('get_userInfo2 ', userId);
     const userinfo = useQuery(['get_userInfo', userId], () => userApi.getMyInfo(userId), {
       enabled: !!userId,
     });
