96e00968ef7fcb5d6d9b5bd8813a3f155101e7e2
Harper9808 <60000653+Harper9808@users.noreply.github.com>
2022-08-10 21:11:07 +0900

    [add] https 적용

--- src/Api/baseUrl.ts
+++ src/Api/baseUrl.ts
@@ -4,7 +4,7 @@ import setupInterceptorsTo from './interceptors';
 
 const baseUrl = axios.create({
   // baseURL: process.env.REACT_APP_BASE_URL,
-  baseURL: 'http://huddledown.link/api',
+  baseURL: 'https://huddledown.link/api',
 });
 
 export default setupInterceptorsTo(baseUrl);
