3e6d36a7f74d1e2b6cf0f4afc7192768da088244
Harper9808 <60000653+Harper9808@users.noreply.github.com>
2022-08-10 21:04:28 +0900

    [set] https적용

---
+++ src/Api/baseUrl.tsx
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import setupInterceptorsTo from './interceptors';
+
+const baseUrl = axios.create({
+  // baseURL: process.env.REACT_APP_BASE_URL,
+  // baseURL: 'http://southoftheriver.iptime.org:8080/api',
+  baseURL: 'https://huddledown.link/api',
+});
+
+export default setupInterceptorsTo(baseUrl);
